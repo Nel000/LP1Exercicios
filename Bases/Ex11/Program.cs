f093bead4a4db00beb7a8bd4fe8567ce1207eeee
Manuel Geraldes <45165444+Nel000@users.noreply.github.com>
2022-02-24 16:00:31 +0000

    Get user input as string and reverse it

---
+++ Bases/Ex11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ex11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The user string
+            string userString;
+
+            // Writes an introductory message
+            Console.WriteLine("Please write something");
+
+            // Saves user input as string
+            userString = Console.ReadLine();
+
+            // FOR loop goes from last to first character of user string
+            for (int i = userString.Length - 1; i >= 0; i--)
+                // Prints each letter from the string from last to first
+                Console.Write(userString[i]);
+        }
+    }
+}
