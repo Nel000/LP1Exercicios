a9369f754833512426675d40139ed011febb5ce4
Manuel Geraldes <45165444+Nel000@users.noreply.github.com>
2022-02-24 15:17:03 +0000

    Update file structure

---
+++ Bases/Exs123/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exs123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
