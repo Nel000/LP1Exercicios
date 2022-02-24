287925f856e3c9f961d5f127e5aec4656df74da3
Manuel Geraldes <45165444+Nel000@users.noreply.github.com>
2022-02-24 15:43:20 +0000

    Add project for exs 456789

---
+++ Bases/Exs456789/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exs456789
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ex4
+            uint n = uint.MaxValue;
+            Console.WriteLine(n + 1);
+        }
+    }
+}
