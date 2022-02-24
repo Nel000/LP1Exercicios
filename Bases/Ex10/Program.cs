bfa8ee65400c706c3819bd6e8a55c1595d913f26
Manuel Geraldes <45165444+Nel000@users.noreply.github.com>
2022-02-24 15:49:18 +0000

    Read input as string and print it back

---
+++ Bases/Ex10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The string to be filled by the user
+            string userString;
+
+            // Introductory message
+            Console.WriteLine("Please write a string");
+            // Read user input and save onto string
+            userString = Console.ReadLine();
+            
+            // Print back user input as string
+            Console.WriteLine($"User entered string: {userString}");
+        }
+    }
+}
