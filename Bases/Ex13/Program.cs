d21c5eef5559d4997babce4ee70dfdb353887eb8
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-02-27 15:34:47 +0000

    Calculate result of double elevated to another from user input

---
+++ Bases/Ex13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ex13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string si, sj;
+            double i, j;
+
+            // Initial message
+            Console.WriteLine("Insert 2 floating point numbers");
+
+            // Store user input in strings
+            si = Console.ReadLine();
+            sj = Console.ReadLine();
+
+            // Convert strings to float
+            i = Convert.ToDouble(si);
+            j = Convert.ToDouble(sj);
+
+            // Print result of the operation
+            Console.WriteLine($"{i} elevated to {j} equals {Math.Pow(i, j)}");
+        }
+    }
+}
