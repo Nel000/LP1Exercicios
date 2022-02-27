fca5025785fb1066800de572889ba0f4ebb0d350
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-02-27 15:52:07 +0000

    Add program that prints each parameter from command line

---
+++ Bases/Ex15/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ex15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The Length property provides the number of array elements
+            System.Console.WriteLine("parameter count = {0}", args.Length);
+
+            // FOR loop goes through each arg
+            for (int i = 0; i < args.Length; i++)
+            {
+                // Print each arg in order
+                System.Console.WriteLine("Arg[{0}] = [{1}]", i, args[i]);
+            }
+        }
+    }
+}
