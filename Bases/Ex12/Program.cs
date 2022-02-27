d304af49f28f2e3b5e489e9c49bbbf92cf11c54e
Manuel Geraldes <45165444+Nel000@users.noreply.github.com>
2022-02-27 15:20:44 +0000

    Add program that prints sum between 2 user set ints

---
+++ Bases/Ex12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ex12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables to be defined by the user
+            string i , j;
+            int ii, ij;
+
+            // Introductory message
+            Console.WriteLine("Write 2 integers:");
+            
+            // Store user input in string
+            i = Console.ReadLine();
+            j = Console.ReadLine();
+
+            // Convert strings to int
+            ii = Convert.ToInt32(i);
+            ij = Convert.ToInt32(j);
+
+            // Print sum of 2 ints
+            Console.WriteLine($"Sum between {ii} and {ij} is {ii + ij}");
+        }
+    }
+}
