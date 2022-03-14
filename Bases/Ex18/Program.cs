78c574692b68a590c733953dc80a3716ab59f577
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-03-14 19:16:10 +0000

    Get user input as string and print back

---
+++ Bases/Ex18/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Stores user input as string
+            string s;
+
+            // Request user input
+            Console.WriteLine("Please write a sentence");
+
+            // Get user input
+            s = Console.ReadLine();
+
+            // Print user input
+            Console.WriteLine($"String = {s}");
+        }
+    }
+}
