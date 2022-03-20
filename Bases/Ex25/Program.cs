c249681eaf52ecad500783e045c8a556dd20604c
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-03-20 12:22:50 +0000

    Rewrite code without 'breaks' and 'continues'

---
+++ Bases/Ex25/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ex25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int maxTurns = 20;
+            int turn = 0;
+
+            while (turn < maxTurns)
+            {
+                turn++;
+
+                if (turn % 3 != 0)
+                {
+                    Console.WriteLine($"Turn number {turn}");
+                }
+            }
+
+            Console.WriteLine($"Last turn was {turn}");
+        }
+    }
+}
