378f26185b5e15fd19865f09bc08125be14c613b
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-03-21 21:09:57 +0000

    Complete Triang method, call from Main

---
+++ Bases/Ex45/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ex45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = Convert.ToInt32(Console.ReadLine());
+
+            int blocks = Triang(lines);
+
+            Console.WriteLine(blocks);
+        }
+
+        static int Triang(int lines)
+        {
+            int blocks = 0;
+
+            if (lines > 0)
+                // Ex: 4
+                // (4 - 1) + [4]; (3 - 1) + [3]; (2 - 1) + [2]; (1 - 1) + [1];
+                // = 10
+                blocks = Triang(lines - 1) + lines;
+            else
+                lines = 0;
+            
+            return blocks;
+        }
+    }
+}
