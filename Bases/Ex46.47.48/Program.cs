db40149e96a6a4522fc19ada3339bc5f72ce1e68
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-03-21 21:29:43 +0000

    Rename project for exercises 46, 47 and 48

--- Bases/Ex46.47.48/Program.cs
+++ Bases/Ex46.47.48/Program.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 
-namespace Ex46
+namespace Ex46._47._48
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // 46
+            //Console.WriteLine(MetodoRecursivo(5));
             // 5 = 0? N; 5 % 2 = 0? N; [2+] 4 = 0? N; 4 % 2 = 0? S; [3+] 3...
             // [2+] 2 % 2 = 0? S; [3+] 1 % 2 = 0? N; [2+] 0 (STOP) = [12]
-            Console.WriteLine(MetodoRecursivo(5));
+
+            // 47
+            uint n = 5;
+            Console.WriteLine($"{n}! = {Fact(n)}");
         }
 
         static int MetodoRecursivo(int n)
@@ -20,5 +25,7 @@ namespace Ex46
             else
                 return 2 + MetodoRecursivo(n - 1);
         }
+
+        static uint Fact(uint i) => i > 1 ? i * Fact(i - 1) : 1;
     }
 }
