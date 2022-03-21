34539b6f696783f8dffc360b3b3a48cbe12cc911
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-03-21 21:34:36 +0000

    Add solution for exercise 47

--- Bases/Ex46.47.48/Program.cs
+++ Bases/Ex46.47.48/Program.cs
@@ -6,14 +6,17 @@ namespace Ex46._47._48
     {
         static void Main(string[] args)
         {
-            // 46
+            // 46)
             //Console.WriteLine(MetodoRecursivo(5));
             // 5 = 0? N; 5 % 2 = 0? N; [2+] 4 = 0? N; 4 % 2 = 0? S; [3+] 3...
             // [2+] 2 % 2 = 0? S; [3+] 1 % 2 = 0? N; [2+] 0 (STOP) = [12]
 
-            // 47
+            // 47)
             uint n = 5;
             Console.WriteLine($"{n}! = {Fact(n)}");
+            // 5 != 120;
+            // 5 > 1? S; [5*] 4 > 1? S; [4*] 3 > 1? S... 1 > 1? N (STOP)
+            // Factorial de número: 5 * 4 * 3 * 2 * 1 = 120;
         }
 
         static int MetodoRecursivo(int n)
