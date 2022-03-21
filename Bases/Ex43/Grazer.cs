5c201f7b63db1c9dc4912fbdc32488d0acf79dc2
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-03-21 20:20:11 +0000

    Call overloaded methods on Main

--- Bases/Ex43/Grazer.cs
+++ Bases/Ex43/Grazer.cs
@@ -4,14 +4,14 @@ namespace Ex43
 {
     public class Grazer
     {
-        private static int Eat(Terrain t)
+        public static int Eat(Terrain t)
         {
             int i = (int) t;
 
             return i;
         }
 
-        private static int Eat(BonusTerrain bt)
+        public static int Eat(BonusTerrain bt)
         {
             int i = (int) bt * 10;
 
--- Bases/Ex43/Program.cs
+++ Bases/Ex43/Program.cs
@@ -6,7 +6,17 @@ namespace Ex43
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Terrains:");
+            Console.WriteLine($"-{Grazer.Eat(Terrain.Rock)}");
+            Console.WriteLine($"-{Grazer.Eat(Terrain.Plains)}");
+            Console.WriteLine($"-{Grazer.Eat(Terrain.Forest)}");
+            Console.WriteLine($"-{Grazer.Eat(Terrain.Jungle)}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Other terrains:");
+            Console.WriteLine($"-{Grazer.Eat(BonusTerrain.Water)}");
+            Console.WriteLine($"-{Grazer.Eat(BonusTerrain.Food)}");
         }
     }
 }
