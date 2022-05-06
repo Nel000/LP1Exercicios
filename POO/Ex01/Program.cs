336d58cc1868755c1139a3eca3da761dc67fe191
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-05-06 19:08:19 +0100

    Solve exercise 1

---
+++ POO/Ex01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minValue, maxValue;
+
+            int[] randNums = new int[5];
+
+            Random rand = new Random();
+
+            minValue = Convert.ToInt32(args[0]);
+            maxValue = Convert.ToInt32(args[1]);
+
+            if (minValue > maxValue)
+            {
+                int temp = minValue;
+                minValue = maxValue;
+                maxValue = temp;
+            }
+
+            for (int i = 0; i < randNums.Length; i++)
+            {
+                randNums[i] = rand.Next(minValue, maxValue + 1);
+                Console.WriteLine(randNums[i]);
+            }
+        }
+    }
+}
