55dc23f1eb4a0ea5f3b189bfe9861d10ac2386bd
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-05-08 21:37:27 +0100

    Add files for Ex37

---
+++ POO/Ex37/NPC.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ex37
+{
+    public abstract class NPC
+    {
+        public float HP { get; protected set; }
+
+        public NPC(float hp)
+        {
+            HP = hp;
+        }
+
+        public void PlayTurn()
+        {
+            if (FindEnemies())
+            {
+                AttackEnemies();
+            }
+            if (FindFood())
+            {
+                EatFood();
+            }
+            Move();
+        }
+
+        protected abstract bool FindFood();
+        protected abstract bool FindEnemies();
+        protected abstract void EatFood();
+        protected abstract void AttackEnemies();
+
+        protected virtual void Move()
+        {
+            Console.WriteLine(this.GetType() + " has moved!");
+        }
+    }
+}
